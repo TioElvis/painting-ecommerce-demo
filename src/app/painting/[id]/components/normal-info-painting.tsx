70515f3c2544b0f14f18bb483a20fa5ec4e1e362
TioElvis <elvis.veramijares@gmail.com>
2026-04-25 01:21:34 +0200

    Fix height

--- src/app/painting/[id]/components/normal-info-painting.tsx
+++ src/app/painting/[id]/components/normal-info-painting.tsx
@@ -32,7 +32,7 @@ export function NormalInfoPainting({ painting }: Props) {
   return (
     <div className="w-full h-full flex items-center justify-center">
       <section className="w-full flex items-center justify-center">
-        <div className="w-xl h-140 flex flex-col justify-between bg-background/20 backdrop-blur-3xl rounded-l-lg p-4 text-white">
+        <div className="w-xl h-120 flex flex-col justify-between bg-background/20 backdrop-blur-3xl rounded-l-lg p-4 text-white">
           <div>
             <div className="flex items-center justify-between">
               <h2 className="text-5xl font-bold">{painting.title}</h2>
@@ -50,13 +50,13 @@ export function NormalInfoPainting({ painting }: Props) {
             <h2 className="text-xl font-bold">{painting.date}</h2>
           </div>
         </div>
-        <div className="w-xl h-140 relative">
+        <div className="w-xl h-150">
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
             id="card-image"
             src={painting.image}
             alt={painting.title}
-            className="w-xl h-180 absolute rounded-lg -top-20"
+            className="w-full h-full rounded-lg"
           />
         </div>
       </section>
