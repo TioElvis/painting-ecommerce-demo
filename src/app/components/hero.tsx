90721af980c1511d275ce1e49d1d28f39c99f4e8
TioElvis <elvis.veramijares@gmail.com>
2026-04-24 21:23:08 +0200

    Add hero component

---
+++ src/app/components/hero.tsx
@@ -0,0 +1,68 @@
+"use client";
+import { gsap } from "gsap";
+import { useEffect, useState } from "react";
+
+import { useIsBelow } from "@/hooks/use-is-below";
+import { MaxWidthWrapper } from "@/components/max-width-wrapper";
+
+export function Hero() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  const isBelow768 = useIsBelow(768);
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setIsMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (!isMounted) return;
+
+    gsap.set(["#text1", "#text2"], { clearProps: "all" });
+
+    const tl = gsap.timeline();
+
+    if (isBelow768) {
+      tl.fromTo(
+        "#text1",
+        { x: -10, opacity: 0 },
+        { x: 0, opacity: 1, duration: 1, ease: "power3.out", delay: 1.5 },
+      ).fromTo(
+        "#text2",
+        { x: 10, opacity: 0 },
+        { x: 0, opacity: 1, duration: 1, ease: "power3.out" },
+        "<",
+      );
+    } else {
+      tl.fromTo(
+        "#text1",
+        { x: -30, opacity: 0 },
+        { x: 0, opacity: 1, duration: 1, ease: "power3.out", delay: 1.5 },
+      ).fromTo(
+        "#text2",
+        { x: 30, opacity: 0 },
+        { x: 0, opacity: 1, duration: 1, ease: "power3.out" },
+        "<",
+      );
+    }
+  }, [isBelow768, isMounted]);
+
+  return (
+    <MaxWidthWrapper className="flex flex-col gap-4 sm:gap-0 sm:flex-row sm:justify-between">
+      <p id="text1" className="opacity-0">
+        <span className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl">
+          For the
+        </span>
+        <br />
+        <span className="text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-bold">
+          art lovers
+        </span>
+      </p>
+      <p id="text2" className="sm:text-right opacity-0">
+        This is a demo for an e-commerce that sells painting. <br />
+        You can find various paintings here. <br />
+        We provide high quality paintings for art lovers.
+      </p>
+    </MaxWidthWrapper>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-"use client";
 import { Fragment } from "react/jsx-runtime";
 
+import { Hero } from "./components/hero";
 import { Header } from "./components/header";
 
 export default function Page() {
   return (
     <Fragment>
       <Header withAnimation />
-      <section></section>
+      <Hero />
       <main></main>
     </Fragment>
   );
