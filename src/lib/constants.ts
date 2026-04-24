68aa02bc5fc841c21f684581d319c8694b2c6433
TioElvis <elvis.veramijares@gmail.com>
2026-04-24 21:54:13 +0200

    Add mocked data

---
+++ src/lib/constants.ts
@@ -0,0 +1,38 @@
+export type Painting = {
+  id: string;
+  image: string;
+  title: string;
+};
+
+export const MOCKED_DATA_PAINTINGS: Painting[] = [
+  {
+    id: "1",
+    image: "/ALE03516.jpg",
+    title: "ALE03516",
+  },
+  {
+    id: "2",
+    image: "/ALE03568.jpg",
+    title: "ALE03568",
+  },
+  {
+    id: "3",
+    image: "/ALE03615.jpg",
+    title: "ALE03615",
+  },
+  {
+    id: "4",
+    image: "/ALE03618.jpg",
+    title: "ALE03618",
+  },
+  {
+    id: "5",
+    image: "/Cristiana-3.jpg",
+    title: "Cristiana-3",
+  },
+  {
+    id: "6",
+    image: "/Cristiana-5.jpg",
+    title: "Cristiana-5",
+  },
+];
