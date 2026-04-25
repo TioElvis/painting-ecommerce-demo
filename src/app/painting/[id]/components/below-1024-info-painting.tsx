3deb12654456e1bc51de2829886975046d47d222
TioElvis <elvis.veramijares@gmail.com>
2026-04-25 14:39:21 +0200

    Change text font size values for responsive

--- src/app/painting/[id]/components/below-1024-info-painting.tsx
+++ src/app/painting/[id]/components/below-1024-info-painting.tsx
@@ -34,7 +34,7 @@ export function Below1024InfoPainting({ painting }: Props) {
   return (
     <div className="w-full h-full flex items-center justify-center py-4 md:py-8 lg:py-12">
       <section className="w-full md:w-lg h-full flex flex-col items-center justify-center">
-        <div className="w-full h-96">
+        <div className="w-full h-88 md:h-96">
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
             id="card-image"
@@ -43,22 +43,26 @@ export function Below1024InfoPainting({ painting }: Props) {
             className="w-full h-full rounded-t-lg"
           />
         </div>
-        <div className="h-80 text-white bg-background/20 backdrop-blur-3xl rounded-b-lg p-4 flex flex-col justify-between">
+        <div className="h-72 md:h-80 text-white bg-background/20 backdrop-blur-3xl rounded-b-lg p-4 flex flex-col justify-between">
           <div>
             <div className="flex items-center justify-between">
-              <h2 className="text-4xl font-bold">{painting.title}</h2>
+              <h2 className="text-2xl md:text-4xl font-bold">
+                {painting.title}
+              </h2>
               <Badge variant="secondary">
                 {painting.forSale ? "For Sale" : "Not For Sale"}
               </Badge>
             </div>
-            <p className="text-lg">{painting.description}</p>
-            <p className="text-2xl font-bold">${painting.price.toFixed(2)}</p>
+            <p className="md:text-lg">{painting.description}</p>
+            <p className="text-xl md:text-2xl font-bold">
+              ${painting.price.toFixed(2)}
+            </p>
             <Button size="lg" variant="outline" className="cursor-pointer w-52">
               Add to Cart
             </Button>
           </div>
           <div className="flex items-center justify-between">
-            <h2 className="text-xl font-bold">{painting.date}</h2>
+            <h2 className="text-lg md:text-xl font-bold">{painting.date}</h2>
           </div>
         </div>
       </section>
